86971f14e0f061a55d84555952633936e0abc066
sylph03 <sylph203@gmail.com>
2025-09-15 10:37:03 +0700

    chore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./components/Layout"
+import Layout from "./components/layout"
 import Blog from "./pages/BLog"
 function App() {
 
